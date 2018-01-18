d183d504c0ea1c3387a3e2a700b3c038ec704e16
Stef Bon <stefbon@gmail.com>
2018-01-18 12:15:47 +0100

    Remove hash table for desktop entries.

--- fuse-fs-special.c
+++ fuse-fs-special.c
@@ -68,32 +68,26 @@ static char *desktopentryname=".directory";
 static pthread_mutex_t desktopmutex=PTHREAD_MUTEX_INITIALIZER;
 static struct statfs statfs_keep;
 
-#define _SPECIAL_TYPE_OVERLAY			1
+static void _fs_special_forget(struct inode_s *inode)
+{
+    union datalink_u *link=get_datalink(inode);
 
-struct special_file_s {
-    unsigned int			type;
-    struct entry_s			*parent;
-    struct special_file_s		*next;
-    struct special_file_s		*prev;
-    char				*name;
-    struct inode_s			inode;
-    union {
-	char				*target;
-    } backend;
-};
+    if (link && link->data) {
 
-static struct special_file_s *desktopentrytable[64];
+	free(link->data);
+	link->data=NULL;
+
+    }
+
+}
 
 static void _fs_special_getattr(struct service_context_s *context, struct fuse_request_s *request, struct inode_s *inode)
 {
-    logoutput("_fs_special_getattr");
     _fs_common_getattr(context, request, inode);
 }
 
 static void _fs_special_setattr(struct service_context_s *context, struct fuse_request_s *request, struct inode_s *inode, struct stat *st, unsigned int set)
 {
-    logoutput("_fs_special_setattr");
-
     if (set & (FATTR_SIZE | FATTR_UID | FATTR_GID | FATTR_MODE | FATTR_MTIME | FATTR_CTIME)) {
 
 	reply_VFS_error(request, EPERM);
@@ -118,33 +112,27 @@ static void _fs_special_open(struct fuse_openfile_s *openfile, struct fuse_reque
 {
     unsigned int error=EIO;
     struct inode_s *inode=openfile->inode;
-    struct special_file_s *special=(struct special_file_s *) ( ((char *) inode) - offsetof(struct special_file_s, inode));
+    union datalink_u *link=get_datalink(inode);
 
-    logoutput("_fs_special_open");
+    if (link && link->data) {
+	int fd=0;
 
-    if (special) {
+	fd=open(link->data, flags);
 
-	if (special->type==_SPECIAL_TYPE_OVERLAY) {
-	    int fd=0;
+	if (fd>0) {
+	    struct fuse_open_out open_out;
 
-	    fd=open(special->backend.target, flags);
+	    openfile->handle.fd=fd;
 
-	    if (fd>0) {
-		struct fuse_open_out open_out;
+	    open_out.fh=(uint64_t) openfile;
+	    open_out.open_flags=0; //FOPEN_KEEP_CACHE;
+	    open_out.padding=0;
+	    reply_VFS_data(request, (char *) &open_out, sizeof(open_out));
+	    return;
 
-		openfile->handle.fd=fd;
+	} else {
 
-		open_out.fh=(uint64_t) openfile;
-		open_out.open_flags=FOPEN_KEEP_CACHE;
-		open_out.padding=0;
-		reply_VFS_data(request, (char *) &open_out, sizeof(open_out));
-		return;
-
-	    } else {
-
-		error=errno;
-
-	    }
+	    error=errno;
 
 	}
 
@@ -158,70 +146,58 @@ static void _fs_special_open(struct fuse_openfile_s *openfile, struct fuse_reque
 void _fs_special_read(struct fuse_openfile_s *openfile, struct fuse_request_s *request, size_t size, off_t off, unsigned int flags, uint64_t lock_owner)
 {
     unsigned int error=EIO;
-    unsigned char buffer[size];
+    char buffer[size];
     ssize_t read=0;
 
     read=pread(openfile->handle.fd, (void *) buffer, size, off);
 
-    logoutput("_fs_special_read: read %i", read);
-
     if (read==-1) {
 
 	error=errno;
-
-    } else {
-
-	reply_VFS_data(request, buffer, read);
+	reply_VFS_error(request, error);
 	return;
 
     }
 
-    reply_VFS_error(request, error);
+    reply_VFS_data(request, buffer, read);
 
 }
 
 void _fs_special_write(struct fuse_openfile_s *openfile, struct fuse_request_s *request, const char *buffer, size_t size, off_t off, unsigned int flags, uint64_t lock_owner)
 {
-    logoutput("_fs_special_write");
     reply_VFS_error(request, EPERM);
 }
 
 void _fs_special_flush(struct fuse_openfile_s *openfile, struct fuse_request_s *request, uint64_t lock_owner)
 {
-    logoutput("_fs_special_flush");
     reply_VFS_error(request, 0);
 }
 
 void _fs_special_fsync(struct fuse_openfile_s *openfile, struct fuse_request_s *request, unsigned char sync)
 {
-    logoutput("_fs_special_fsync");
     reply_VFS_error(request, 0);
 }
 
 void _fs_special_release(struct fuse_openfile_s *openfile, struct fuse_request_s *request, unsigned int flags, uint64_t lock_owner)
 {
-    logoutput("_fs_special_release");
     close(openfile->handle.fd);
+    openfile->handle.fd=0;
     reply_VFS_error(request, 0);
 }
 
 void _fs_special_fsetattr(struct fuse_openfile_s *openfile, struct fuse_request_s *request, struct stat *st, int fuse_set)
 {
-    logoutput("_fs_special_fsetattr");
     _fs_special_setattr(openfile->context, request, openfile->inode, st, fuse_set);
 }
 
 void _fs_special_fgetattr(struct fuse_openfile_s *openfile, struct fuse_request_s *request)
 {
-    logoutput("_fs_special_fgetattr");
     _fs_special_getattr(openfile->context, request, openfile->inode);
 }
 
 static void _fs_special_statfs(struct service_context_s *context, struct fuse_request_s *request, struct inode_s *inode)
 {
     struct fuse_statfs_out statfs_out;
-
-    logoutput("_fs_special_statfs");
 
     memset(&statfs_out, 0, sizeof(struct fuse_statfs_out));
 
@@ -244,12 +220,12 @@ static void _fs_special_statfs(struct service_context_s *context, struct fuse_re
 
 void init_special_fs()
 {
-    unsigned int error=0;
 
     statfs("/", &statfs_keep);
 
     set_virtual_fs(&fs);
 
+    fs.forget=_fs_special_forget;
     fs.getattr=_fs_special_getattr;
     fs.setattr=_fs_special_setattr;
     fs.type.nondir.open=_fs_special_open;
@@ -261,8 +237,6 @@ void init_special_fs()
     fs.type.nondir.fsetattr=_fs_special_fsetattr;
     fs.type.nondir.fgetattr=_fs_special_fgetattr;
     fs.statfs=_fs_special_statfs;
-
-    for (unsigned int i=0; i<64; i++) desktopentrytable[i]=NULL;
 
 }
 
@@ -277,36 +251,6 @@ void set_fs_special(struct inode_s *inode)
 
 }
 
-struct special_file_s *lookup_desktopentry(uint64_t ino)
-{
-    unsigned int hash=ino % 64;
-    struct special_file_s *special=desktopentrytable[hash];
-
-    while(special) {
-
-	if (special->parent->inode->ino==ino) break;
-	special=special->next;
-
-    }
-
-    return special;
-}
-
-void add_desktopentry(struct special_file_s *special)
-{
-    unsigned int hash=special->parent->inode->ino % 64;
-
-    if (desktopentrytable[hash]) {
-
-	special->next=desktopentrytable[hash];
-	desktopentrytable[hash]->prev=special;
-
-    }
-
-    desktopentrytable[hash]=special;
-
-}
-
 void create_desktopentry_file(char *path, struct entry_s *parent, struct workspace_mount_s *workspace)
 {
     struct stat st;
@@ -314,122 +258,26 @@ void create_desktopentry_file(char *path, struct entry_s *parent, struct workspa
     logoutput("create_desktopentry_file: path %s", path);
 
     if (lstat(path, &st)==0 && S_ISREG(st.st_mode)) {
-	struct special_file_s *special=NULL;
+	struct name_s xname;
+	unsigned int error=0;
 
-	pthread_mutex_lock(&desktopmutex);
+	xname.name=desktopentryname;
+	xname.len=strlen(xname.name);
+	calculate_nameindex(&xname);
 
-	special=lookup_desktopentry(parent->inode->ino);
+	struct entry_s *entry=_fs_common_create_entry_unlocked(workspace, parent, &xname, &st, 0, &error);
 
-	if (! special) {
-	    char *target=NULL;
+	if (entry) {
+	    union datalink_u link;
 
-	    special=malloc(sizeof(struct special_file_s));
-	    target=strdup(path);
+	    entry->inode->fs=&fs;
 
-	    if (special && target) {
-		struct inode_s *inode=&special->inode;
-
-		init_inode(inode);
-
-		special->name=desktopentryname;
-		special->parent=parent;
-		special->next=NULL;
-		special->prev=NULL;
-
-		inode->mode=st.st_mode;
-		inode->uid=st.st_uid;
-		inode->gid=st.st_gid;
-		inode->nlink=1;
-
-		inode->size=st.st_size;
-		inode->fs=&fs;
-
-		get_current_time(&inode->mtim);
-		memcpy(&inode->ctim, &inode->mtim, sizeof(struct timespec));
-		memcpy(&inode->atim, &inode->mtim, sizeof(struct timespec));
-
-		special->type=_SPECIAL_TYPE_OVERLAY;
-		special->backend.target=target;
-
-		add_desktopentry(special);
-		add_inode_hashtable(inode, increase_inodes_workspace, (void *) workspace);
-
-	    } else {
-
-		if (special) {
-
-		    free(special);
-		    special=NULL;
-
-		}
-
-		if (target) {
-
-		    free(target);
-		    target=NULL;
-
-		}
-
-	    }
-
-	}
-
-	pthread_mutex_unlock(&desktopmutex);
-
-    }
-
-}
-
-void special_file_lookup(struct service_context_s *context, struct entry_s *parent, struct fuse_request_s *request)
-{
-    struct special_file_s *special=NULL;
-
-    logoutput("special_file_lookup");
-
-    pthread_mutex_lock(&desktopmutex);
-
-    special=lookup_desktopentry(parent->inode->ino);
-
-    if (special) {
-	struct inode_s *inode=&special->inode;
-
-	inode->nlookup++;
-	logoutput("special_file_lookup: found");
-	_fs_common_cached_lookup(context, request, inode);
-
-    } else {
-
-	reply_VFS_error(request, ENOENT);
-
-    }
-
-    pthread_mutex_unlock(&desktopmutex);
-
-}
-
-void remove_special_files()
-{
-    struct special_file_s *special=NULL;
-
-    for (unsigned int i=0; i<64; i++) {
-
-	special=desktopentrytable[i];
-
-	while (special) {
-
-	    desktopentrytable[i]=special->next;
-
-	    if (special->type==_SPECIAL_TYPE_OVERLAY) {
-
-		if (special->backend.target) free(special->backend.target);
-
-	    }
-
-	    free(special);
-	    special=desktopentrytable[i];
+	    link.data=strdup(path);
+	    set_datalink(entry->inode, &link);
 
 	}
 
     }
 
 }
+
